2be14418e4a0c4d56a0ffc47c45c6bf05d4ba504
linxc_legion <201198403@qq.com>
2026-07-22 16:08:16 +0800

    chore: 版本更新至1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.scoreviewer.msv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
